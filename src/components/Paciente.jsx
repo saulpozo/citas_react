38c60d0e81db23785c89b1a9adfa9bddf5adc07c
Saul <pozoantonsaulbrayan@gmail.com>
2023-06-04 13:00:15 -0500

    Primer commit

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,53 @@
+import {useEffect} from "react"
+
+const Paciente = ({paciente, setPaciente,eliminarPaciente}) => {
+    const {nombre, propietario, email, fecha, sintomas, id} = paciente 
+
+    const handleEliminar = () => {
+        const respuesta = confirm('Deseas eliminar este paciente');
+
+        if(respuesta) {
+            eliminarPaciente(id)
+        }
+    }
+
+    return (
+        <div className="mx-5 my-10 bg-white shadow-md rounded-xl px-5 py-10">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{' '}
+                <span className="font-normal normal-case">{nombre}</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{' '}
+                <span className="font-normal normal-case">{propietario}</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email:{' '}
+                <span className="font-normal normal-case">{email}</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta:{' '}
+                <span className="font-normal normal-case">{fecha}</span>
+            </p>
+
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas:{' '}
+                <span className="font-normal normal-case">{sintomas}</span>
+            </p>
+
+            <div className="flex justify-around mt-10">
+                <button 
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-900 text-white font-bold uppercase rounded"
+                    onClick = {() => setPaciente(paciente)}
+                >Editar</button>
+
+                <button 
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-900 text-white font-bold uppercase rounded-lg"
+                    onClick={handleEliminar}
+                >Eliminar</button>
+            </div>
+        </div>
+    )
+}
+
+export default Paciente
